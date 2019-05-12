0c68fbcef4c15065329c7e7828198966ec0a2abd
Tiffany Zhong <tiffzhong@gmail.com>
2019-05-12 00:33:32 -0700

    vowels

--- vowels/index.js
+++ vowels/index.js
@@ -7,6 +7,15 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  let vowels = ["a", "e", "i", "o", "u"];
+  let count = 0;
+  for (let char of str.toLowerCase()) {
+    if (vowels.includes(char)) {
+      count++;
+    }
+  }
+  return count;
+}
 
 module.exports = vowels;
